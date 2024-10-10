97d127a7bf20eef8e263ff7586e23a988d681975
SeungJi <kwag57@naver.com>
2024-10-10 14:28:39 +0900

    create Jodit-Test Project

---
+++ src/main/java/com/cannon/jodittest/JoditTestApplication.java
@@ -0,0 +1,13 @@
+package com.cannon.jodittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoditTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoditTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cannon/jodittest/JoditTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cannon.jodittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoditTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
